02a19c81dfbb428f0fc9d984aaf4c01f47a0cced
mercurydevs <97065316+mmercurydevs@users.noreply.github.com>
2023-08-22 00:52:45 +0000

    desafio completo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>Ich liebe dich, lieben Mutter</h1>
+
+    return <div>
+                <font color="green">
+                    <center><h1>Te amo ❤️</h1></center>
+                </font>
+                <center><img width={"450px"} src="https://i.pinimg.com/1200x/3d/96/66/3d966686e48d7a5cd71e9e4cb8f1e43a.jpg"></img></center>
+            </div>
 }
 
 export default Home;
