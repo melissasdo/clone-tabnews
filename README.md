1ee84c7c68347a66ced7f70cdb858e680662e089
mercurydevs <97065316+mmercurydevs@users.noreply.github.com>
2023-08-29 01:06:25 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do https://tabnews.com.br do https:/curso.dev. Projeto de teste :)
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 function Home() {
-
-    return <div>
-                <font color="green">
-                    <center><h1>Te amo ❤️</h1></center>
-                </font>
-                <center><img width={"450px"} src="https://i.pinimg.com/1200x/3d/96/66/3d966686e48d7a5cd71e9e4cb8f1e43a.jpg"></img></center>
-            </div>
+  return (
+    <div>
+      <font color="green">
+        <center>
+          <h1>Te amo ❤️</h1>
+        </center>
+      </font>
+      <center>
+        <img
+          width={"450px"}
+          src="https://i.pinimg.com/1200x/3d/96/66/3d966686e48d7a5cd71e9e4cb8f1e43a.jpg"
+        ></img>
+      </center>
+    </div>
+  );
 }
 
 export default Home;
