3096f372fffe9d054b1c2dba9831a40cee6f7017
Melissa Oliveira <97065316+melissasdo@users.noreply.github.com>
2023-09-23 20:45:59 +0000

    add `/api/v1/status` endpoint

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "df68cf7daa183b21"; }
+/******/ 		__webpack_require__.h = function() { return "218ca31e8149dd20"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.6dd7f9dd32a23930.hot-update.js
+++ .next/static/webpack/webpack.6dd7f9dd32a23930.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "63216c90de0f40d5"; }
+/******/ 	__webpack_require__.h = function() { return "b517fc903bd6a004"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/make namespace object */
