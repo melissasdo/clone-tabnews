e6787b25fbd28667e0b87906be35bdfffd8caabf
Melissa Oliveira <97065316+melissasdo@users.noreply.github.com>
2024-07-29 23:08:24 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(req, res) {
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 AS SUM;");
+  console.log(result.rows);
   res
     .status(200)
     .json({ chave: "alunos do curso.dev são pessoas acima da média" });
